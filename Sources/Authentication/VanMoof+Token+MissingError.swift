5da722a3a60644470fa13f63ee5005d9e8b748ef
Sven Tiigi <sven.tiigi@gmail.com>
2022-11-22 08:25:30 +0100

    Use Swift.Error

--- Sources/Authentication/VanMoof+Token+MissingError.swift
+++ Sources/Authentication/VanMoof+Token+MissingError.swift
@@ -5,7 +5,7 @@ import Foundation
 public extension VanMoof.Token {
     
     /// A VanMoof Token Missing Error
-    struct MissingError: Codable, Hashable, Error {
+    struct MissingError: Codable, Hashable, Swift.Error {
         
         /// Creates a new instance of `VanMoof.Token.MissingError`
         public init() {}
