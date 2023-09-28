f4e5cd8a489843cc2b854d1ae1bbf03d55931b4a
Sven Tiigi <sven.tiigi@gmail.com>
2023-09-28 18:00:45 +0200

    Added VanMoofBikeBluetoothReadWritableCharacteristic conformance

--- Sources/VanMoofKit/Bike/Bluetooth/Services/Firmware/VanMoof+Bike+BluetoothServices+Firmware+BlockCharacteristic.swift
+++ Sources/VanMoofKit/Bike/Bluetooth/Services/Firmware/VanMoof+Bike+BluetoothServices+Firmware+BlockCharacteristic.swift
@@ -5,9 +5,33 @@ import Foundation
 extension VanMoof.Bike.BluetoothServices.Firmware {
     
     /// The Block Characteristic.
-    enum BlockCharacteristic: VanMoofBikeBluetoothCharacteristic {
-        /// The identifier.
-        static let id: String = "6ACC5512-E631-4069-944D-B8CA7598AD50"
+    struct BlockCharacteristic {
+        
+        /// The data.
+        let data: Data
+        
+    }
+    
+}
+
+// MARK: - VanMoofBikeBluetoothReadWritableCharacteristic
+
+extension VanMoof.Bike.BluetoothServices.Firmware.BlockCharacteristic: VanMoofBikeBluetoothReadWritableCharacteristic {
+    
+    /// The identifier.
+    static let id: String = "6ACC5512-E631-4069-944D-B8CA7598AD50"
+    
+    /// The writable bluetooth data.
+    var writableData: VanMoof.Bike.BluetoothData {
+        .init(self.data)
+    }
+    
+    /// Creates a new instance from VanMoof Bike BluetoothData, if available
+    /// - Parameter data: The VanMoof Bike BluetoothData
+    init?(data: VanMoof.Bike.BluetoothData) {
+        self.init(
+            data: data.rawValue
+        )
     }
     
 }
